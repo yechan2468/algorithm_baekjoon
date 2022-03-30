2245b74942e32b23b4ea9400904625a340c18ee3
Yechan Lee <yechan2468@naver.com>
2022-03-30 22:26:17 +0900

    Create 1927.py

---
+++ 1927.py
@@ -0,0 +1,18 @@
+import heapq
+import sys
+
+
+# https://www.acmicpc.net/problem/1927
+
+n = int(sys.stdin.readline())
+arr = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        if len(arr) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(arr))
+        continue
+    heapq.heappush(arr, num)
